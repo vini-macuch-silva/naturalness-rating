ebc8654b9a172bbb48d25181e6ffe359986dae8a
Vinicius MacUch Silva (English Language and Linguistics) <v.macuchsilva@bham.ac.uk>
2024-01-18 12:46:33 +0100

    Prolific live test

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -3,9 +3,9 @@ export default {
   serverUrl: 'http://localhost:4000/',
   socketUrl: 'http://localhost:4000/socket',
   // this will be used in prolific mode
-  // completionUrl: 'https://...',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C1IL9M3C',
   contactEmail: 'v.macuchsilva@bham.ak.uk',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
